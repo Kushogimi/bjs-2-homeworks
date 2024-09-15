1f31a676cd2de42c7224034c1afcd0adb305d3c1
Michael Kulikov <minumpie@mail.ru>
2024-09-15 14:49:48 +0300

    First Commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,18 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discriminant = b ** 2 - 4 * a * c;
+  if (discriminant < 0) {
+    arr = [];
+  } else if (discriminant === 0) {
+    arr = [-b / (2 * a)];
+  } else {
+    arr = [
+      (-b + Math.sqrt(discriminant)) / (2 * a),
+      (-b - Math.sqrt(discriminant)) / (2 * a),
+    ];
+  }
   return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
-}
+function calculateTotalMortgage(percent, contribution, amount, countMonths) {}
