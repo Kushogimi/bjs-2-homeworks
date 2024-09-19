b9ba73bafb44acddf9b1f09d33e5e36e5b060a58
Kushogimi <86256222+Kushogimi@users.noreply.github.com>
2024-09-19 23:56:03 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -15,37 +15,53 @@ function getArrayParams(...arr) {
 }
 
 function summElementsWorker(...arr) {
+  if (arr.length !== 0) {
     return arr.reduce((acc, item, idx, mass) => {
       return acc += item;
     })
+  } else {
+    return 0;
   }
-
+}
+  
 function differenceMaxMinWorker(...arr) {
+  if (arr.length !== 0) {
     return Math.max(...arr) - Math.min(...arr);
+  } else {
+    return 0;
   }
+}
 
 function differenceEvenOddWorker(...arr) {
+  if (arr.length !== 0) {
     let sumEvenElement = 0, sumOddElement = 0;
-  for (i = 0; i < arr.length; i += 1) {
-    if (arr[i] % 2 === 0) {
-      sumEvenElement += arr[i];
-    } else {
-      sumOddElement += arr[i];
+    for (i = 0; i < arr.length; i += 1) {
+      if (arr[i] % 2 === 0) {
+        sumEvenElement += arr[i];
+      } else {
+        sumOddElement += arr[i];
+      }
     }
+    return sumEvenElement - sumOddElement;
+  } else {
+    return 0;
   }
-  return sumEvenElement - sumOddElement;
-  }
+}
 
 function averageEvenElementsWorker(...arr) {
+  if (arr.length !== 0) {
     let sumEvenElement = 0, countEvenElement = 0;
-  for (i = 0; i < arr.length; i += 1) {
-    if (arr[i] % 2 === 0) {
-      sumEvenElement += arr[i];
-      countEvenElement += 1;
+    for (i = 0; i < arr.length; i += 1) {
+      if (arr[i] % 2 === 0) {
+        sumEvenElement += arr[i];
+        countEvenElement += 1;
+      }
     }
-  }
-  return sumEvenElement / countEvenElement;
-  }
+    return sumEvenElement / countEvenElement;
+  } else {
+    return 0;
+  } 
+}
 
 function makeWork (arrOfArr, func) {
   let maxWorkerResult = -Infinity;
